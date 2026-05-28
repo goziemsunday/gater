abece1417cc82970661b5fa690b72348ada177d1
Chiagoziem Sunday <gozmansunday@gmail.com>
2026-05-28 18:25:50 +0100

    fix: handle 'route not found' and 'method not allowed' errors

--- cmd/server/api.go
+++ cmd/server/api.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/chiagxziem/gater/internal/config"
+	"github.com/chiagxziem/gater/internal/json"
 	"github.com/chiagxziem/gater/internal/mailer"
 	"github.com/chiagxziem/gater/internal/store"
 	"github.com/chiagxziem/gater/internal/validator"
@@ -58,6 +59,13 @@ func (a *application) mount() http.Handler {
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Use(a.injectLogging)
+
+	r.NotFound(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		json.WriteError(w, http.StatusNotFound, "route not found")
+	}))
+	r.MethodNotAllowed(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		json.WriteError(w, http.StatusMethodNotAllowed, "method not allowed")
+	}))
 
 	// api routes
 	r.Route("/api", func(r chi.Router) {
